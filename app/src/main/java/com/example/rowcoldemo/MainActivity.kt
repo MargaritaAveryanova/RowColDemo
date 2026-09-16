7126f5f0fdab579373220657eb06481063016909
MargaritaAveryanova <120007228+MargaritaAveryanova@users.noreply.github.com>
2026-09-16 13:12:11 +0700

    вызов MainScreen вместо Greeting

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -10,6 +10,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.rowcoldemo.ui.theme.RowColDemoTheme
 
@@ -19,19 +28,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RowColDemoTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//
-//                }
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(modifier = Modifier.padding(innerPadding))
+
+                }
             }
         }
     }
 }
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun TextCell(text: String, modifier: Modifier = Modifier) {
+    val cellModifier = modifier
+        .padding(4.dp)
+        .size(100.dp, 100.dp)
+        .border(width = 4.dp, color = Color.Black)
+    Text(text = text, cellModifier.then(modifier),
+        fontSize = 80.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center)
+}
+
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     RowColDemoTheme {
-
+        MainScreen()
     }
 }
